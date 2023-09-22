406adb1a364ff9e187e820f624a451ecd57b3020
Elnus <sunle8@sohu.com>
2023-09-22 07:37:53 +0000

    重写对话函数，支持连续对话

--- pkg/ErnieBot-Turbo/wenxin.go
+++ pkg/ErnieBot-Turbo/wenxin.go
@@ -5,118 +5,88 @@ import (
 	"fmt"
 	"gotalk/pkg/conf"
 	"io"
-	"log"
 	"net/http"
 	"strings"
 )
 
 type Request struct {
-	Message      []Message `json:"message"`
-	PenaltyScore *float64  `json:"penalty_score,omitempty"`
-	Stream       *bool     `json:"stream,omitempty"`
-	Temperature  *float64  `json:"temperature,omitempty"`
-	TopP         *float64  `json:"top_p,omitempty"`
-	UserID       string    `json:"user_id"`
+	Messages     []Messages `json:"messages"`
+	PenaltyScore *float64   `json:"penalty_score,omitempty"`
+	Stream       *bool      `json:"stream,omitempty"`
+	Temperature  *float64   `json:"temperature,omitempty"`
+	TopP         *float64   `json:"top_p,omitempty"`
+	UserID       string     `json:"user_id"`
 }
 
 var Payload Request
 
-type TokenBody struct {
-	RefreshToken  string `json:"refresh_token"`
-	ExpireIn      int    `json:"expires_in"`
-	SessionKey    string `json:"session_key"`
-	AccessToken   string `json:"access_token"`
-	Scope         string `json:"scope"`
-	SessionSecret string `json:"session_secret"`
-}
-
-var TokenBodys TokenBody
-
-type Message struct {
-	Content string `json:"content"`
+type Messages struct {
 	Role    string `json:"role"`
+	Content string `json:"content"`
 }
 
-const (
-	tokenApi = "https://aip.baidubce.com/oauth/2.0/token"
-	chatApi  = "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/eb-instant"
-)
+const ()
 
-// var request Request
 var (
-	access_token  = ""
 	client_secret = conf.Configs.BceAPP.ClientSecret
 	client_id     = conf.Configs.BceAPP.ClientID
-	charUrl       = ""
-	tokenUrl      = ""
 )
 
 func init() {
-	//access_token = GetToken()
-	charUrl = fmt.Sprintf("%s?access_token=%s", chatApi, access_token)
-	tokenUrl = fmt.Sprintf("%s?grant_type=client_credentials&client_id=%s&client_secret=%s", tokenApi, client_id, client_secret)
 }
 
-func DoChat() []byte {
+var Jsmess []byte
 
-	method := "POST"
+func DoChat(content string) string {
+	um := &Messages{Content: content, Role: "user"}
+	Payload.Messages = append(Payload.Messages, *um)
+	Jsmess, _ = json.Marshal(Payload)
 
-	payload := strings.NewReader(`{
-		"messages": [
-			{
-				"role": "user",
-				"content": "hello"
-			}
-		]
-	}`)
-
+	url := "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/eb-instant?access_token=" + GetAccessToken()
+	payload := strings.NewReader(string(Jsmess))
 	client := &http.Client{}
-	req, err := http.NewRequest(method, charUrl, payload)
+	req, err := http.NewRequest("POST", url, payload)
 
 	if err != nil {
-		log.Println(err)
-		return nil
+		fmt.Println(err)
+		return ""
 	}
 	req.Header.Add("Content-Type", "application/json")
 
 	res, err := client.Do(req)
 	if err != nil {
-		log.Println(err)
-		return nil
+		fmt.Println(err)
+		return ""
 	}
 	defer res.Body.Close()
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		log.Println(err)
-		return nil
+		fmt.Println(err)
+		return ""
 	}
-	return body
+	chatObj := map[string]string{}
+	json.Unmarshal([]byte(body), &chatObj)
+	am := &Messages{Content: chatObj["result"], Role: "assistant"}
+	Payload.Messages = append(Payload.Messages, *am)
+	return chatObj["result"]
 }
 
-func GetToken() {
-
-	method := "GET"
-
-	client := &http.Client{}
-	req, err := http.NewRequest(method, tokenUrl, nil)
-
+func GetAccessToken() string {
+	url := "https://aip.baidubce.com/oauth/2.0/token"
+	postData := fmt.Sprintf("grant_type=client_credentials&client_id=%s&client_secret=%s", client_id, client_secret)
+	resp, err := http.Post(url, "application/x-www-form-urlencoded", strings.NewReader(postData))
 	if err != nil {
-		log.Println(err)
-		return
+		fmt.Println(err)
+		return ""
 	}
-
-	res, err := client.Do(req)
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Println(err)
-		return
+		fmt.Println(err)
+		return ""
 	}
-	defer res.Body.Close()
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	json.Unmarshal(body, &TokenBodys)
+	accessTokenObj := map[string]string{}
+	json.Unmarshal([]byte(body), &accessTokenObj)
+	return accessTokenObj["access_token"]
 }
