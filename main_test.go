af282ac16bedd981517a39594c7e7c529b98dd2f
Elnus <sunle8@sohu.com>
2023-09-22 07:38:07 +0000

    测试连续对话

--- main_test.go
+++ main_test.go
@@ -7,6 +7,8 @@ import (
 )
 
 func TestMain(t *testing.T) {
-	wenxin.GetToken()
-	fmt.Println(wenxin.TokenBodys.AccessToken)
+	res1 := wenxin.DoChat("hello")
+	fmt.Println("对话结果:", res1)
+	res2 := wenxin.DoChat("介绍一下你自己")
+	fmt.Println("对话结果:", res2)
 }
